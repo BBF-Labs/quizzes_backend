9def94691b3677de053bfed9d45ffb18197d86a4
michaelperryjnr <michaelperryt97@gmail.com>
2025-01-16 14:55:58 +0000

    Update import routes

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,4 @@
 import { authorizeRoles, authenticateUser } from "./authMiddlewares";
-import { Limiter, sessionMiddleware } from "./sessionMiddleware";
+import { Limiter, sessionMiddleware } from "./sessionMiddlewares";
 
 export { Limiter, sessionMiddleware, authenticateUser, authorizeRoles };
