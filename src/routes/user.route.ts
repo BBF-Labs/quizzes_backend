139507ff312f0917dcfae0a68afe17ce8b2b45c0
michaelperryjnr <michaelperryt97@gmail.com>
2025-01-18 23:17:45 +0000

    Finished user routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from "express";
-import { createUser } from "../controllers";
+import { createUser, findUserByEmail, updateUser } from "../controllers";
+import { authGuard, authenticateUser } from "../middlewares";
+import { findUserByUsername } from "../controllers";
+import { Config, StatusCodes } from "../config";
+import { IUser } from "../interfaces";
 
 const userRoutes: Router = Router();
 
@@ -8,19 +12,99 @@ userRoutes.post("/register", async (req: Request, res: Response) => {
     const user = req.body;
 
     if (!user) {
-      res.status(400).json({ message: "Invalid request" });
+      res.status(StatusCodes.BAD_REQUEST).json({ message: "Invalid request" });
+      return;
     }
 
     const newUser = await createUser(user);
 
     if (!newUser) {
-      res.status(400).json({ message: "User already exists" });
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "User already exists" });
+      return;
     }
 
-    res.status(201).json(newUser);
+    res.status(StatusCodes.CREATED).json({ message: "Success", newUser });
   } catch (err: any) {
-    res.status(500).json({ message: err.message });
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Error", error: err.message });
   }
 });
+
+userRoutes.get(
+  "/profile",
+  authenticateUser,
+  authGuard,
+  async (req: Request, res: Response) => {
+    try {
+      const user = req.user as IUser;
+
+      if (!user) {
+        res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json({ message: "User not found" });
+        return;
+      }
+
+      const userDoc = user.email
+        ? await findUserByEmail(user.email)
+        : await findUserByUsername(user.username!);
+
+      if (!userDoc) {
+        res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ message: "User profile not found" });
+        return;
+      }
+
+      res.status(StatusCodes.OK).json({ message: "Success", userDoc });
+    } catch (err: any) {
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "Error", error: err.message });
+    }
+  }
+);
+
+userRoutes.put(
+  "/update",
+  authenticateUser,
+  authGuard,
+  async (req: Request, res: Response) => {
+    try {
+      const user = req.user as IUser;
+
+      if (!user) {
+        res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json({ message: "You are not authorized to perform this action" });
+      }
+
+      const userDoc = await findUserByUsername(user.username!);
+
+      if (!userDoc) {
+        res.status(StatusCodes.NOT_FOUND).json({ message: "User not found" });
+        return;
+      }
+      const updates = req.body;
+
+      if (!updates) {
+        res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ message: "Invalid Request" });
+      }
+
+      await updateUser(userDoc.id, updates);
+
+      res.status(StatusCodes.OK).json({ message: "User updated successfully" });
+    } catch (err: any) {
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "Error", error: err.message });
+    }
+  }
+);
 
 export default userRoutes;
