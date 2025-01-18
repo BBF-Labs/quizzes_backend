73486e873671bca0f9e604da9ca97527d8ff00b1
michaelperryjnr <michaelperryt97@gmail.com>
2025-01-18 19:44:36 +0000

    Update types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -3,7 +3,20 @@ import { IUser } from "../interfaces";
 
 declare global {
   namespace Express {
-    interface User extends Partial<IUser> {}
+    interface User {
+      id?: string;
+      username?: string;
+      email?: string;
+      password?: string;
+      authKey?: string;
+      course?: Types.ObjectId;
+      role?: "student" | "admin" | "moderator";
+      isBanned?: boolean;
+      isSubscribed?: boolean;
+      paymentId?: Types.ObjectId;
+      lastLogin?: Date;
+      isDeleted?: boolean;
+    }
   }
 }
 
