6eebf332915e3054d42294d27f916ef65b68df5f
michaelperryjnr <michaelperryt97@gmail.com>
2025-01-19 22:01:06 +0000

    Update middlewares to read from session

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,11 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import passport from "passport";
 import * as passportStrategy from "passport-local";
-import {
-  verifyToken,
-  findUserByUsername,
-  verifyPassword,
-} from "../controllers";
+import { findUserByUsername, verifyPassword } from "../controllers";
 import { IUser } from "../interfaces";
 import { StatusCodes } from "../config";
 
@@ -14,45 +10,25 @@ async function authenticateUser(
   res: Response,
   next: NextFunction
 ) {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .json({ message: "Token is required" });
-  }
-
-  const token = authHeader.split(" ")[1];
-
   try {
-    const tokenUser = await verifyToken(token);
-
-    if (!tokenUser) {
-      return res
-        .status(StatusCodes.UNAUTHORIZED)
-        .json({ message: "Invalid Token" });
-    }
-
-    const user = await findUserByUsername(tokenUser.email);
+    const user = req.session.user;
 
     if (!user) {
-      return res
-        .status(StatusCodes.UNAUTHORIZED)
-        .json({ message: "User not found" });
+      res.status(StatusCodes.UNAUTHORIZED).json({ message: "" });
+      return;
     }
 
-    req.user = user;
+    const userDoc = await findUserByUsername(user.username);
+
+    if (!userDoc) {
+      res.status(StatusCodes.UNAUTHORIZED).json({ message: "User not found" });
+      return;
+    }
     next();
   } catch (err: any) {
-    if (err.name === "TokenExpiredError") {
-      res.status(StatusCodes.UNAUTHORIZED).json({ message: "Token expired" });
-    } else if (err.name === "JsonWebTokenError") {
-      res.status(StatusCodes.UNAUTHORIZED).json({ message: "Invalid token" });
-    } else {
-      res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ message: "Internal server error" });
-    }
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "There was an error processing request" });
   }
 }
 
@@ -107,9 +83,10 @@ passport.serializeUser((user: Partial<IUser>, done) => {
   done(null, sessionUser);
 });
 
-passport.deserializeUser(async (sessionUser: any, done) => {
+passport.deserializeUser(async (sessionUser: { username: string }, done) => {
   try {
     const user = await findUserByUsername(sessionUser.username);
+
     if (user) {
       done(null, user);
     } else {
