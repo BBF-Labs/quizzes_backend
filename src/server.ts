ca08a126ac0b16905e6d22a58de2ffed7bd46178
michaelperryjnr <michaelperryt97@gmail.com>
2025-01-21 16:54:49 +0000

    Added single reverse proxy support

--- src/server.ts
+++ src/server.ts
@@ -31,6 +31,7 @@ async function startServer() {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.disable("x-powered-by");
+    app.set("trust proxy", 1);
     app.use(Session);
     app.use(Limiter);
     app.use(helmet());
