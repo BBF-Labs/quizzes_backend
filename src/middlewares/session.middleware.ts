e5814d8726b649a9052ca8823f9715013c57d5e3
michaelperryjnr <michaelperryt97@gmail.com>
2025-01-23 00:51:21 +0000

    Update session middleware

--- src/middlewares/session.middleware.ts
+++ src/middlewares/session.middleware.ts
@@ -24,21 +24,38 @@ const genUUID = () => {
 
 const startSession = async () => {
   await connectToDB();
+
+  const sessionStore = MongoStore.create({
+    client: mongoose.connection.getClient(),
+    collectionName: "sessions",
+    ttl: 60 * 60 * 24, // 1 day
+    autoRemove: "native",
+    touchAfter: 24 * 3600,
+    crypto: {
+      secret: Config.SESSION_SECRET,
+    },
+  });
+
+  sessionStore.on("error", (error) => {
+    if (Config.ENV === "development") {
+      console.error("Session Store Error: ", error);
+    }
+    console.error("Session Store Error");
+  });
   const Session = session({
     secret: Config.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false, // Set to false for production
+    saveUninitialized: true,
+    rolling: true,
+    name: "sid",
     cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+      sameSite: "lax",
       secure: Config.ENV === "production",
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
     },
-    genid: function () {
-      return genUUID();
-    },
-    store: MongoStore.create({
-      client: mongoose.connection.getClient(),
-    }),
+    store: sessionStore,
+    genid: genUUID,
   });
 
   return Session;
