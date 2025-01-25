acdd73d0f2b1974f3556b6dec0b65166e5b4f7e0
michaelperryjnr <michaelperryt97@gmail.com>
2025-01-25 22:03:07 +0000

    Update models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,9 @@
-export {default as User} from "./user.model"
-export {default as Questions} from "./questions.model"
-export {default as Question} from "./question.model"
-export {default as Quiz} from "./quiz.model"
-export {default as Course} from "./course.model"
-export {default as Material} from "./material.model"
-export {default as Progress} from "./progress.model"
-export {default as Payment} from "./payment.model"
-export {default as Package} from "./package.model"
+export { default as User } from "./user.model";
+export { default as QuizQuestion } from "./quizquestion.model";
+export { default as Question } from "./question.model";
+export { default as Quiz } from "./quiz.model";
+export { default as Course } from "./course.model";
+export { default as Material } from "./material.model";
+export { default as Progress } from "./progress.model";
+export { default as Payment } from "./payment.model";
+export { default as Package } from "./package.model";
