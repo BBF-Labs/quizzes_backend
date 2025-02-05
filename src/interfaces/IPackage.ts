3233d9c6002ae2d3376454c90f03a4fac7c7a9c3
Michael Perry Tettey <michaelperryt97@gmail.com>
2025-02-05 02:04:52 -0800

    Update IPackage.ts

--- src/interfaces/IPackage.ts
+++ src/interfaces/IPackage.ts
@@ -4,7 +4,7 @@ interface IPackage extends Document {
   _id: Types.ObjectId;
   price: number;
   name: string;
-  duration: number | "lifetime";
+  duration: number | "lifetime" | "daily" | "quarterly";
   isUpgradable?: boolean;
   numberOfCourses?: number;
   courses?: Types.ObjectId[];
