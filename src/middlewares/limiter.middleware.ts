ad79686e767d8817e3306ba90f36694e7fcc2807
michaelperryjnr <michaelperryt97@gmail.com>
2025-02-13 22:20:58 +0000

    Update rate limit

--- src/middlewares/limiter.middleware.ts
+++ src/middlewares/limiter.middleware.ts
@@ -1,6 +1,6 @@
 import rateLimit from "express-rate-limit";
 const Limiter = rateLimit({
-  windowMs: 5 * 60 * 1000,
+  windowMs: 6 * 60 * 1000,
   limit: 200,
   message: { error: "Too many requests, please try again later" },
   handler: (req, res) => {
