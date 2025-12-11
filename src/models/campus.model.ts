f5d5de000ccbfde67066172f686ce71ea76bfc55
Joy Owusu Ansah <owusujoyansah@gmail.com>
2025-12-11 12:10:18 +0000

    Completed the campus model

--- src/models/campus.model.ts
+++ src/models/campus.model.ts
@@ -5,14 +5,51 @@ const CampusSchema = new Schema<ICampus>(
   {
     name: {
       type: String,
+      required: true,
     },
     code: {
       type: String,
+      unique: true,
+      required: true,
     },
-    schoolId: [{
+    schoolId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "School",
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    allowResourceSharing: {
+      type: Boolean,
+      default: true,
+    },
+    sharedWithCampuses: {
       type: [Schema.Types.ObjectId],
-      ref: 'SchoolId',
-    }],
-
+      ref: "Campus",
+    },
+    students: {
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+    },
+    courses: {
+      type: [Schema.Types.ObjectId],
+      ref:"Course",
+    },
+    admins: {
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
   }
-)
+);
+
+const Campus : Model<ICampus> = model<ICampus>("Campus", CampusSchema);
+export default Campus;
